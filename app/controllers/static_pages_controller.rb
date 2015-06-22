b9eb4b6e418e2081c1efe440bcfbe0d6c3f8c134
Phil <p.clauzel90@gmail.com>
2015-06-22 19:23:36 +0100

    First commit to gym site

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,27 @@
+class StaticPagesController < ApplicationController
+
+  def home
+  end
+
+  def membership
+  end
+
+  def times
+  end
+
+  def classes
+  end
+
+  def facilities
+  end
+
+  def contact
+  end
+
+  def boxing
+  end
+
+  def personal_training
+  end
+
+end
---
+++ public/fonts.js
@@ -0,0 +1,2 @@
+  $('body').append($('<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">'));
+  $('body').append($('<link href="http://fonts.googleapis.com/css?family=Francois+One" rel="stylesheet" type="text/css">'));
